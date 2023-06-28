a5e1b393e069c84932c3e0b8c5d0073d34b4f0d2
Ki_Lop <ilovevy159@gmail.com>
2023-06-28 22:33:46 +0700

    kilopdev

--- src/services/api/request.js
+++ src/services/api/request.js
@@ -1,4 +1,4 @@
-const API_DOMAIN = "django-test-server-h0a8srdbv-kilopfd.vercel.app/";
+const API_DOMAIN = "https://django-test-server-h0a8srdbv-kilopfd.vercel.app/";
 
 export const get = async (path, options = {}) => {
     const response = await fetch(API_DOMAIN + path, options);
