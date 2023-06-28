4fa15581e28459f6b597ea22484bd7d07ccd2df5
Ki_Lop <ilovevy159@gmail.com>
2023-06-28 22:46:39 +0700

    kilopdev

--- src/services/api/getLesson.js
+++ src/services/api/getLesson.js
@@ -4,6 +4,7 @@ const api_url = 'api'
 
 
 export const lesson = async (params, categories = 'cpp') => {
+
     const data = await get(`${api_url}/lesson/${categories}/${params}`,)
     return data
 }
