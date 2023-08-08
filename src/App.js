e3c68392ab98ac378241f3afee0e8fee924f30b1
Ki_Lop <tuan8165@gmail.com>
2023-08-08 10:59:25 +0700

    p

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import './App.css';
 import { AllRoutesPages } from './services/routes';
 
 function App() {
+  // disable right click
+  document.addEventListener('contextmenu', event => event.preventDefault());
+ 
+  document.onkeydown = function (e) {
+
+      // disable F12 key
+      if(e.keyCode == 123) {
+          return false;
+      }
+
+      // disable I key
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+          return false;
+      }
+
+      // disable J key
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+          return false;
+      }
+
+      // disable U key
+      if(e.ctrlKey && e.keyCode == 85) {
+          return false;
+      }
+  }
   return (
     <>
       <AllRoutesPages/>
